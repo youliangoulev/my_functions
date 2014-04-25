812cbd3128bfd34618d5d61124723904e3de104f
youliangoulev <yolian.goulev@gmail.com>
2014-04-25 20:18:09 +0200

    q

--- growth/plotgrowth.m
+++ growth/plotgrowth.m
@@ -9,6 +9,7 @@ delta={};
 size={};
 zd=[];
 za=[];
+xt=[];
 for i=1:length(deltaini)
    if ~isempty(deltaini{i})
        xax=[xax , (i-1)*framet];
@@ -16,6 +17,7 @@ for i=1:length(deltaini)
        size={size{:} , sizeini{i}};
        zd=[zd , deltaini{i} ];
        za=[za , sizeini{i}];
+       xt=[xt , zeros(1, length(sizeini{i}))+(i-1)*framet ];%randn(1, length(sizeini{i}))+(i-1)*framet ];
    end;
 end;
 meandelta=[];
@@ -34,6 +36,7 @@ for i=1:length(delta)
     grpersiz=[grpersiz , sum(delta{i})/sum(size{i})];
     errorgrpersiz=[errorgrpersiz , sqrt((length(delta{i})*std(delta{i})*std(delta{i})/sum(size{i})^2)+((sum(delta{i})^2)*length(delta{i})*std(size{i})*std(size{i})/sum(size{i})^4))];
 end;
+%=====================================================
 figure;
 errorbar(xax , smooth(meandelta , sms) , errordelta);
 hold on;
@@ -42,12 +45,23 @@ set(gca,'FontSize',20)
 xlabel('Time (min)');
 ylabel('Mean growth rate per cell (au)');
 title('Cell growth rate'); 
+%=====================================================
 figure;
-plot(xax , smooth(meansize , sms));
+errorbar(xax , smooth(meansize , sms) , errorsize);
+hold on;
+plot(xax , smooth(meansize , sms) , 'color' , 'r' , 'LineWidth' , 2);
 set(gca,'FontSize',20)
 xlabel('Time (min)');
 ylabel('Mean volum per cell (au)');
-title('Cell area');
+title('Cell volum');
+%=====================================================
+figure;
+scatter(xt , za , 7 , 'b' , 'Marker' , '+');
+set(gca,'FontSize',20)
+xlabel('Time (min)');
+ylabel('Volum per cell (au)');
+title('Cell volum distribution');
+%=====================================================
 figure;
 errorbar(xax , smooth(grpersiz , sms) , errorgrpersiz);
 hold on;
@@ -56,18 +70,21 @@ set(gca,'FontSize',20)
 xlabel('Time (min)');
 ylabel('Mean growth rate per unit of volum (min-1)');
 title('Growth rate per volum'); 
+%=====================================================
 figure;
 plot(xax , num , 'LineStyle' , 'none' , 'MarkerSize', 6 , 'MarkerEdgeColor','k' , 'MarkerFaceColor','g' , 'Marker' , 's');
 set(gca,'FontSize',20)
 xlabel('Time (min)');
 ylabel('Number of analysed events');
 title('Statistical significance');
+%=====================================================
 figure ;
 scatter (za , zd) ;
 set(gca,'FontSize',20)
 xlabel('Cell volum (au)');
 ylabel('Cell growth rate (au)');
 title('Cell area and growth rate correlation');
+%=====================================================
 stop1=false;
 stop2=false;
 stop3=false;
